a0bac2252573b15cb0cecbecd9fc412fc177f9a8
marissaverdonck <marissa.verdonck@hva.nl>
2020-01-09 12:56:34 +0100

    search

--- script/script.js
+++ script/script.js
@@ -1,67 +1,70 @@
-// Fetch geeft toegang tot het json file
-// .then wacht tot de data binnen is, anders crash
-fetch('data.json')
-  .then(response => response.json())
-  .then(json => getData(json));
-
-function getData(data) {
-  console.log(data)
-
-  function getCompanyByName(bedrijfsnaam) {
-    return data.filter(
-      function(data) {
-        return data.bedrijfsnaam == bedrijfsnaam
-      }
-    );
-  }
-
-  function getCompanyByPlace(plaats) {
-    return data.filter(
-      function(data) {
-        return data.plaats == plaats
-      }
-    );
-  }
-
-  // var foundName = getCompanyByName('Zuidoostzorg');
-  // document.getElementById('output1').innerHTML = foundName[0].bedrijfsnaam
-  // console.log(foundName);
-
-  var foundPlace = getCompanyByPlace('HEEMSKERK');
-  var ul = d3.select('body').append('ul');
-  ul.selectAll('li')
-    .data(foundPlace)
-    .enter()
-    .append("li")
-    .append("a")
-    .attr("xlink:href", function(foundPlace) {
-      return foundPlace.concerncode
-    })
+// After click search button get the value from the field
+function getInputSearchField() {
+  const inputText =
+    document.getElementById("inputText").value;
+  console.log(inputText)
+    // // fill in the value in p-element
+    // document.getElementById(
+    //   "output2").innerHTML = inputText;
 
 
 
+  // Fetch geeft toegang tot het json file
+  // .then wacht tot de data binnen is, anders crash
+  fetch('data.json')
+    .then(response => response.json())
+    .then(json => getData(json));
 
-  .text(function(foundPlace) {
-      return foundPlace.bedrijfsnaam + ", " + foundPlace.plaats + ", " +
-        foundPlace.concerncode;
 
+
+  function getData(data) {
+    console.log(data)
+
+    // Find by Name
+    // function getCompanyByName(bedrijfsnaam) {
+    //   return data.filter(
+    //     function(data) {
+    //       return data.bedrijfsnaam == bedrijfsnaam
+    //     }
+    //   );
+    // }
+    // var foundName = getCompanyByName('Zuidoostzorg');
+    // document.getElementById('output1').innerHTML = foundName[0].bedrijfsnaam
+    // console.log(foundName);
+
+    // Find by Place
+
+
+    function getCompanyByPlace(plaats) {
+
+      return data.filter(
+        function(data) {
+          return data.plaats == plaats
+        }
+      );
     }
 
+    var foundPlace = getCompanyByPlace(inputText);
+    var ul = d3.select('body').append('ul');
+    ul.selectAll('li')
+      .data(foundPlace)
+      .enter()
+      .append("li")
+      .append("a")
+      .attr("xlink:href", function(foundPlace) {
+        return "http://zorgcowboys/" + foundPlace.concerncode + ".com"
+      })
+      // Open new tab
+      .on('click', function(foundPlace) {
+        window.open("http://zorgcowboys/" + foundPlace.concerncode + ".com")
+      })
+      .text(function(foundPlace) {
+        return foundPlace.bedrijfsnaam + ", " + foundPlace.plaats;
+      })
 
-  )
-
-
-
-
-
-
+  }
 }
-
-
-
-
-
-
+// Without D3
 // var foundPlace = getCompanyByPlace('HEEMSKERK');
 // for (let i = 0; i < foundPlace.length; i++) {
 //   var para = document.createElement("P");
