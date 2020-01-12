23b0cdc6d4d4d95b09dded47dafc2c3982aa0293
marissaverdonck <marissa.verdonck@hva.nl>
2020-01-12 16:41:11 +0100

    resultpage d3, css

--- script/script.js
+++ script/script.js
@@ -1,6 +1,4 @@
-var list = d3.select('#results').append('ul');
-// const searchButton = document.getElementById("searchButton");
-
+var section = d3.select('#results');
 
 // After clicking the search button, get the value from the field
 function getInputSearchField() {
@@ -29,38 +27,154 @@ function getInputSearchField() {
       );
     }
 
-    // update li elements
-    var text = list.selectAll('li').data(foundPlace)
-    text
+    // d3 elements
+    var article = section.selectAll('p').data(foundPlace).enter().append('article');
+    var allArticles = section.selectAll('article').data(foundPlace)
+    var article_a = section.selectAll('a').data(foundPlace)
+    var article_h2 = article_a.selectAll('a > h2').data(foundPlace)
+    var article_h3 = d3.selectAll('article > h3').data(foundPlace)
+    var article_p = d3.selectAll('article > p').data(foundPlace)
+    var article_imgAlert = d3.selectAll('#alert').data(foundPlace)
+
+    // Update elements
+    article_a
       .attr("xlink:href", function(foundPlace) {
-        return "http://zorgcowboys/" + foundPlace.concerncode + ".com"
+        return "http://zorgcowboys/" + foundPlace.plaats + foundPlace.concerncode + ".com"
       })
+    article_h2
+      .attr('id', 'article_h2')
       .text(function(foundPlace) {
-        return foundPlace.bedrijfsnaam + ", " + foundPlace.plaats;
+        return foundPlace.bedrijfsnaam;
       })
       // Open new tab
       .on('click', function(foundPlace) {
         window.open("http://zorgcowboys/" + foundPlace.concerncode + ".com")
-      });
-    // exit li elements
-    text
+      })
+    article_h3
+      .text(function(foundPlace) {
+        return foundPlace.plaats;
+      })
+    article_p
+      .text(function(foundPlace) {
+        return "Winstpercentage: " + foundPlace.perc_winst + "%";
+      })
+    article_imgAlert
+      .attr("src", function(foundPlace) {
+        if (foundPlace.perc_winst > 10) {
+          return "images/icons/alert_solid.png";
+        } else if (foundPlace.perc_winst < 10) {
+          return "images/icons/check_solid.png";
+        } else {
+          return "images/icons/Percentage.png";
+        }
+      })
+
+    // Exit  elements
+    allArticles
       .exit()
       .remove();
-    // enter li elements
-    text
-      .enter()
-      .append('li')
+
+    // Enter elements
+    article
       .append('a')
       .attr("xlink:href", function(foundPlace) {
-        return "http://zorgcowboys/" + foundPlace.concerncode + ".com"
+        return "http://zorgcowboys/" + foundPlace.plaats + foundPlace.concerncode + ".com"
       })
+      .append('h2')
+      .attr('id', 'article_h2')
       .text(function(foundPlace) {
-        return foundPlace.bedrijfsnaam + ", " + foundPlace.plaats;
+        return foundPlace.bedrijfsnaam;
       })
       // Open new tab
       .on('click', function(foundPlace) {
         window.open("http://zorgcowboys/" + foundPlace.concerncode + ".com")
-      });
+      })
+    article
+      .append('h3')
+      .text(function(foundPlace) {
+        return foundPlace.plaats;
+      })
+    article
+      .append('img')
+      .attr('class', 'winst')
+      .attr("src", "images/icons/money_purple.png")
+
+    article
+      .append('p')
+      .attr('class', 'winst')
+      .text(function(foundPlace) {
+        return "Winstpercentage: " + foundPlace.perc_winst + "%";
+      })
+    article
+      .append('img')
+      .attr('class', 'winst')
+      .attr('id', 'alert')
+      .attr("src", function(foundPlace) {
+        if (foundPlace.perc_winst > 10) {
+          return "images/icons/alert_solid.png";
+        } else if (foundPlace.perc_winst < 10) {
+          return "images/icons/check_solid.png";
+        } else {
+          return "images/icons/Percentage.png";
+        }
+      })
+    article
+      .append('div')
+
+
+    article
+      .append('img')
+      .attr('class', 'loon')
+      .attr("src", "images/icons/wallet_purple.png")
+
+    article
+      .append('p')
+      .attr('class', 'loon')
+      .text(function(foundPlace) {
+        return "Percentage loon: " + foundPlace.perc_loon + "%";
+      })
+    article
+      .append('img')
+      .attr('class', 'loon')
+      .attr('id', 'alert')
+      .attr("src", function(foundPlace) {
+        if (foundPlace.perc_loon > 40) {
+          return "images/icons/alert_solid.png";
+        } else if (foundPlace.perc_loon < 40) {
+          return "images/icons/check_solid.png";
+        } else {
+          return "images/icons/Percentage.png";
+        }
+      })
+    article
+      .append('div')
+
+    article
+      .append('img')
+      .attr('class', 'fte')
+      .attr("src", "images/icons/user_purple.png")
+
+    article
+      .append('p')
+      .attr('class', 'fte')
+      .text(function(foundPlace) {
+        return "Omzet per FTE: €" + foundPlace.omzet_fte;
+      })
+    article
+      .append('img')
+      .attr('class', 'fte')
+      .attr('id', 'alert')
+      .attr("src", function(foundPlace) {
+        if (foundPlace.omzet_fte > 125000) {
+          return "images/icons/alert_solid.png";
+        } else if (foundPlace.omzet_fte < 125000) {
+          return "images/icons/check_solid.png";
+        } else {
+          return "images/icons/Percentage.png";
+        }
+      })
+
+
   }
 }
 
