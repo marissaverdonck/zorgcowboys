d2224a9dc7378a35d2ad473f6903b8b3b5bdad21
marissaverdonck <marissa.verdonck@hva.nl>
2020-01-21 14:23:06 +0100

    omzet fte

--- script/detail.js
+++ script/detail.js
@@ -1,23 +1,29 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const concerncode = urlParams.get('concerncode')
-const name = urlParams.get('name')
-const place = urlParams.get('place')
-const winstSVG = d3.select('#winstSVG')
-const loonSVG = d3.select('#loonSVG')
-const jaartal = document.getElementById("jaartal")
-const h2_name = document.getElementById("name")
-const h3_place = document.getElementById("place")
-const width = 280
-const height = 330
-const widthLoon = 280
-const heightLoon = 100
+const concerncode = urlParams.get('concerncode');
+const name = urlParams.get('name');
+const place = urlParams.get('place');
+const winstSVG = d3.select('#winstSVG');
+const loonSVG = d3.select('#loonSVG');
+const fteSVG = d3.select('#fteSVG');
+const jaartal = document.getElementById("jaartal");
+const h2_name = document.getElementById("name");
+const h3_place = document.getElementById("place");
+const width = 280;
+const height = 330;
+const widthLoon = 280;
+const heightLoon = 100;
+const widthFte = 210;
+const heightFte = 300;
 const margin = { top: 50, right: 0, bottom: 20, left: 28 };
 const marginLoon = { top: 45, right: 40, bottom: 5, left: 0 };
+const marginFte = { top: 60, right: 40, bottom: 5, left: 0 };
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 const innerWidthLoon = widthLoon - marginLoon.left - marginLoon.right;
 const innerHeightLoon = heightLoon - marginLoon.top - marginLoon.bottom;
+const innerWidthFte = widthFte - marginFte.left - marginFte.right;
+const innerHeightFte = heightFte - marginFte.top - marginFte.bottom;
 
 h2_name.innerHTML = name
 h3_place.innerHTML = place
@@ -31,7 +37,6 @@ fetch('data.json')
   })
 
 function getData(data) {
-
   function getCompanyData(concerncode) {
     return data.filter(
       function(data) {
@@ -42,39 +47,63 @@ function getData(data) {
   }
   // Algemeen
   const foundData = getCompanyData(concerncode);
-
-
   // Winstpercentage
   const yValue = d => d.perc_winst;
   const xValue = d => d.jaar;
-
   // Loonpercentage
   const yValueLoon = d => d.jaar;
   const xValueLoon = d => d.perc_loon;
-  // Show years in Winstpercentage
+  // Omzet FTE
+  const yValueFte = d => d.omzet_fte;
+  const xValueFte = d => d.jaar;
+  // Show years in title Winstpercentage
   jaartal.innerHTML = (d3.min(foundData, xValue) + "-" + d3.max(foundData, xValue))
 
 
+
+
+
+  // Get data from latetst year and make a new array
   // Loonpercentage
-  // Get the latetst year with perc_loon and make a new array
+  const maxYear = d3.max(foundData, xValue)
   const loonData = new Object();
   const loonDataArray = [loonData]
   var i;
   for (i = 0; i < foundData.length; i++) {
-    if (foundData[i].jaar === d3.max(foundData, yValueLoon) && isNaN(foundData[i].perc_loon) == false) {
+    if (foundData[i].jaar == maxYear && isNaN(foundData[i].perc_loon) == false) {
       loonData.jaar = foundData[i].jaar;
       loonData.perc_loon = foundData[i].perc_loon;
-      console.log(loonData)
-    } else if (foundData[i].jaar === (d3.max(foundData, yValueLoon)) - 1 && isNaN(foundData[i].perc_loon) == false) {
+
+    } else if (foundData[i].jaar == (maxYear - 1) && isNaN(foundData[i].perc_loon) == false) {
       loonData.jaar = foundData[i].jaar;
       loonData.perc_loon = foundData[i].perc_loon;
-      console.log(loonData)
-    } else if (foundData[i].jaar === (d3.max(foundData, yValueLoon)) - 2 && isNaN(foundData[i].perc_loon) == false) {
+
+    } else if (foundData[i].jaar == (maxYear - 2) && isNaN(foundData[i].perc_loon) == false) {
       loonData.jaar = foundData[i].jaar;
       loonData.perc_loon = foundData[i].perc_loon;
-      console.log(loonData)
+
     } else {
-      console.log("onbekend")
+
+    }
+  }
+  // Omzet FTE
+  const fteData = new Object();
+  const fteDataArray = [fteData]
+  var i;
+  for (i = 0; i < foundData.length; i++) {
+    if (foundData[i].jaar == maxYear && isNaN(foundData[i].omzet_fte) == false) {
+      fteData.jaar = foundData[i].jaar;
+      fteData.omzet_fte = foundData[i].omzet_fte;
+
+    } else if (foundData[i].jaar == (maxYear - 1) && isNaN(foundData[i].omzet_fte) == false) {
+      fteData.jaar = foundData[i].jaar;
+      fteData.omzet_fte = foundData[i].omzet_fte;
+
+    } else if (foundData[i].jaar == (maxYear - 2) && isNaN(foundData[i].omzet_fte) == false) {
+      fteData.jaar = foundData[i].jaar;
+      fteData.omzet_fte = foundData[i].omzet_fte;
+
+    } else {
 
     }
   }
@@ -84,7 +113,7 @@ function getData(data) {
     foundData.perc_winst = +foundData.perc_winst;
     foundData.perc_loon = +foundData.perc_loon;
     loonData.perc_loon = +loonData.perc_loon;
-    foundData.omzet_fte = +foundData.omzet_fte;
+    fteData.omzet_fte = +fteData.omzet_fte;
     foundData.jaar = "'" + foundData.jaar.slice(2, 4);;
   })
 
@@ -102,11 +131,21 @@ function getData(data) {
   } else {
     domainValueYMin = 0
   }
-  const maxValueXLoon = loonData.perc_loon;
-  let domainValueXLoon;
-  if (maxValueXLoon < 40) { domainValueXLoon = 40 } else {
-    domainValueXLoon = domainValueXLoon
+  const maxValueYFte = fteData.omzet_fte
+  console.log(maxValueYFte)
+
+  let domainValueYFte;
+  if (maxValueYFte < 130000) { domainValueYFte = 130000 } else {
+    domainValueYFte = maxValueYFte
   }
+
+
+
+  // const maxValueXLoon = loonData.perc_loon;
+  // let domainValueXLoon;
+  // if (maxValueXLoon < 40) { domainValueXLoon = 40 } else {
+  //   domainValueXLoon = domainValueXLoon
+  // }
 
 
 
@@ -129,6 +168,15 @@ function getData(data) {
   const yScaleLoon = d3.scaleBand()
     .domain(0, 1)
     .range([0, innerHeightLoon])
+    // Omzet FTE
+
+  const yScaleFte = d3.scaleLinear()
+    .domain([0, domainValueYFte])
+    .range([innerHeightFte, 0])
+  const xScaleFte = d3.scaleBand()
+    .domain(0, 1)
+    .range([0, innerWidthFte])
+
 
 
 
@@ -223,8 +271,6 @@ function getData(data) {
   // Loonpercentage
   const gLoon = loonSVG.append('g')
     .attr('transform', `translate(${marginLoon.left},${marginLoon.top})`);
-
-
   gLoon.selectAll('rect').data(loonDataArray)
     .enter().append('rect')
     .attr('y', d => yScaleLoon(d.jaar))
@@ -282,5 +328,78 @@ function getData(data) {
   //     .selectAll('.domain')
   //     .attr('stroke', '#d8cedb')
   //     .remove()
+
+
+
+  // Omzet FTE
+  const gFte = fteSVG.append('g')
+    .attr('transform', `translate(${marginFte.left},${marginFte.top})`);
+
+  console.log(fteDataArray)
+  console.log(fteData)
+
+  gFte.selectAll('rect').data(fteDataArray)
+    .enter().append('rect')
+    .attr('x', 20)
+    .attr('width', 160)
+    .attr('height', d => innerHeightFte - yScaleFte(yValueFte(d)))
+    .attr("fill", "#1beaae")
+    .attr('transform', d => `translate(0,${yScaleFte(yValueFte(d))})`);
+
+
+
+  const yAxisGFte = gFte.append('g')
+    .call(d3.axisRight(yScaleFte))
+    .attr('transform', `translate(${innerWidthFte},0)`);
+
+  yAxisGFte
+    .selectAll('.tick line')
+    .attr('x2', -innerWidth)
+    .attr("id", d => "number" + d)
+    .attr('opacity', 0)
+
+  yAxisGFte
+    .selectAll('.tick text')
+    .attr("id", d => "numbertxt" + d)
+    .attr('opacity', 0)
+
+
+  yAxisGFte
+    .select('#number120000')
+    .attr('stroke', '#f65645')
+    .attr('stroke-dasharray', 8)
+    .attr('stroke-width', 2)
+    .attr('opacity', 1)
+
+  yAxisGFte
+    .select('#numbertxt120000')
+    .attr('opacity', 1)
+    .text("€125.000")
+
+
+  yAxisGFte
+    .selectAll('.domain')
+    .remove()
+
+  function thousands_separators(num) {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return num_parts.join(".");
+  }
+
+  yAxisGFte
+    .append('text')
+    .attr('y', innerHeightFte - (innerHeightFte / domainValueYFte * fteData.omzet_fte) + 30)
+    // .attr('y', 0)
+    .attr('x', -innerWidthFte + 50)
+    .attr('id', "titelYFte")
+    .attr('fill', "#6b38e8")
+    .text('€' + thousands_separators(Math.floor(fteData.omzet_fte)) + ',-');
+
+
+
+
+
+
 
 }
